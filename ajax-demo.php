e420a2a436ebbe4a9e6dff45df00cab554092adb
subas-roy <subasroy46@gmail.com>
2026-03-24 15:22:03 +0600

    basic plugin structure

---
+++ ajax-demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Plugin Name: Ajax Demo
+* Description: A simple plugin to demonstrate Ajax in WordPress with shortcode and dashboard widget.
+* Version: 1.0.o
+* Author: Subas Roy
+**/
+
+// Prevent direct access
+defined('ABSPATH') or exit();
+
+class Ajax_Demo{
+    function _construct(){
+        
+    }
+}
+
+new Ajax_Demo();
---
+++ includes/class-shortcode-button.php
@@ -0,0 +1,5 @@
+<?php
+
+class Ajax_Demo_Shortcode_Button {
+    
+}
