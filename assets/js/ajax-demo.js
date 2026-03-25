bc9f376c5b960b19dd515ac8922fe40a5f5f9e9c
subas-roy <subasroy46@gmail.com>
2026-03-25 08:56:25 +0600

    check EvendLintener

--- assets/js/ajax-demo.js
+++ assets/js/ajax-demo.js
@@ -17,4 +17,22 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(jsonData);
         });
     }
+
+    const currencyButton = document.getElementById('ajax-demo-currency-btn');
+    if (currencyButton) {
+        currencyButton.addEventListener('click', async function (event) {
+            console.log('Button Clicked');
+
+            // const formData = new FormData();
+            // formData.append('action', 'demo');
+
+            // const response = await fetch(ajdm.ajax_url, {
+            //     method: 'POST',
+            //     body: formData,
+            // });
+
+            // const jsonData = await response.json();
+            // console.log(jsonData);
+        });
+    }
 });
